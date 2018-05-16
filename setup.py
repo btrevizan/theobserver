25777ddf41330adb1a90779ef71729079b9bb8ae
Bernardo Trevizan <trevizanbernardo@gmail.com>
2018-05-16 16:55:50 -0300

    Update pandas version

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='trevizanbernardo@gmail.com',
     url='https://github.com/btrevizan/theobserver',
     keywords='feature,characteristic extration,machine learning',
-    install_requires=['pandas==0.20.3', 'scikit-learn==0.19.0']
+    install_requires=['pandas==0.23', 'scikit-learn==0.19.0']
 )
